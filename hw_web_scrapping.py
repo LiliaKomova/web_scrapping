5ff806f66bc250ada06e6e8923fe35e8b6a03a4b
Lilia Komova <lilak6051@gmail.com>
2023-04-02 23:21:12 +0300

    add hw

--- hw_web_scrapping.py
+++ hw_web_scrapping.py
@@ -1,42 +1,37 @@
-#  1. Необходимо парсить страницу со свежими вакансиями
-#  с поиском 'Python'и города "Москва" и "Санкт-Петербург".
-#  Эти параметры задаются по ссылке.
-#  2. Нужно выбрать те вакансии, у которых в описании
-#  есть ключевые слова "Django" и "Flask".
-#  3. Записать в json информацию о каждой вакансии
-#  - ссылка,вилка зп, название компании и город.
-
-#  доп.: получать вакансии только с зп в долларах
-
-import requests
 import json
+import requests
 from bs4 import BeautifulSoup
-from pprint import pprint
 
 
-url = 'https://api.hh.ru/vacancies'
-params = {
-    'text': 'python',
-    'area': [1, 2]
-}
+HEADERS = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:103.0) Gecko/20100101 Firefox/103.0'}
+url = f'https://spb.hh.ru/search/vacancy?text=python&area=1&area=2'
 
-response = requests.get(url, params=params)
-data = response.json()
-tag = ['Django', 'Flask']
+response = requests.get(url, headers=HEADERS)
+soup = BeautifulSoup(response.text, 'lxml')
+data = soup.find_all('div', class_='serp-item')
+
 vacancies = {}
-vacancies['vacancy'] = []
 
-for value in data['items']:
-    if value['salary'] is not None and value['salary']['currency'] == 'USD':
-        vacancies['vacancy'].append(
-            {
-                'name': value['name'],
-                'url': value['alternate_url'],
-                'salary': {'from': value['salary']['from'],
-                           'to':value['salary']['to']},
-                'employer': value['employer']['name'],
-                'city': value['area']['name']
-            })
-    pprint(vacancies['vacancy'])
-# with open('data.json', 'w') as filename:
-#     json.dump(vacancies, filename)
+for value in data:
+    s = value.find('span', class_='bloko-header-section-3')
+    if s is not None and 'USD' in s:
+        salary = value.find('span', class_='bloko-header-section-3').text.replace(r'\u202f', ' ')
+        title = value.find('a', class_='serp-item__title').text
+        employer = value.find('a', class_='bloko-link bloko-link_kind-tertiary').text
+        city = value.find('div', attrs={'data-qa': 'vacancy-serp__vacancy-address'}).text
+        url_vacancy = value.find('a', class_='serp-item__title').get('href')
+
+        r = requests.get(url_vacancy,headers=HEADERS)
+        s = BeautifulSoup(r.text, 'lxml')
+
+        description = s.find('div', class_='g-user-content').text.strip()
+        if 'django' in description.lower() and 'flask' in description.lower():
+            vacancies[title] = {
+                'url': url_vacancy,
+                'salary': salary,
+                'employer': employer,
+                'city': city
+            }
+
+with open('vacancies.json', 'w') as filename:
+    json.dump(vacancies, filename)
